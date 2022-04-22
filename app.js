24c48472baf014fa2ff496c0e57346aa2c05b5e6
Brianna Audain <briannaaudain2@outlook.com>
2022-04-22 13:34:28 -0500

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Brianna";
+let lastName = "Audain";
 
+var myAge = 24;
+
+let isDone;
 // Exercise 2
+ fullName = firstName + " " + lastName;
+console.log(fullName);
 
 // Exercise 3
+let myStory = 'My name is ${fullName}. I live in ${city}. I enjoy ${pastime} and reading!';
+
+console.log(myStory);
